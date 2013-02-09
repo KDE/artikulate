7c88486d19990efe8ac2f13b4aa627b5dd39cdc9
Albert Astals Cid <aacid@kde.org>
2013-02-09 17:00:49 +0100

    There is no src/ui folder
    CCMAIL: cola@uni-paderborn.de

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 $EXTRACTRC `find src -name '*.rc'` >> rc.cpp
-$EXTRACTRC `find src/ui -name *.ui` >> rc.cpp
 $EXTRACTRC `find src -name '*.kcfg'` >> rc.cpp
 $XGETTEXT rc.cpp `find src/ -name \*.qml -or -name \*.cpp` -o $podir/artikulate.pot
 
