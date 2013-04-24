e12f332dd7426c0fa5b9aa0ef06abc38f1da324d
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2013-04-24 20:38:40 +0200

    Add translatable names to language files.

--- Messages.sh
+++ Messages.sh
@@ -2,5 +2,5 @@
 $EXTRACTRC `find src -name '*.rc'` >> rc.cpp
 $EXTRACTRC `find src/ui -name *.ui` >> rc.cpp
 $EXTRACTRC `find src -name '*.kcfg'` >> rc.cpp
+$EXTRACTRC --context="Language Title" --tag-group=none --tag=i18nTitle `find data/languages/ -name '*.xml'` >> rc.cpp
 $XGETTEXT rc.cpp `find src/ -name \*.qml -or -name \*.cpp` -o $podir/artikulate.pot
-
