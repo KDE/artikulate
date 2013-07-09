42f30e647398cef3cdd2d4a4896b5206b0f1a350
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2013-07-09 23:11:48 +0200

    Bump version for first contributor release.

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 static const char description[] =
     I18N_NOOP("Learn and practice pronunciation.");
 
-static const char version[] = "0.0.1";
+static const char version[] = "0.1.0";
 
 int main(int argc, char **argv)
 {
