8c91d9c126ec2aef0a5ab9fd6ebbdf5205db634f
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2014-01-04 13:27:49 +0100

    Create Recorder class.
    To be used as QML component.

---
+++ src/core/recorder.cpp
@@ -0,0 +1,98 @@
+/*
+ *  Copyright 2013  Andreas Cord-Landwehr <cordlandwehr@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License or (at your option) version 3 or any later version
+ *  accepted by the membership of KDE e.V. (or its successor approved
+ *  by the membership of KDE e.V.), which shall act as a proxy
+ *  defined in Section 14 of version 3 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "recorder.h"
+#include "capturedevicecontroller.h"
+
+#include <QList>
+#include <KDebug>
+#include <KUrl>
+#include <QString>
+
+Recorder::Recorder(QObject *parent)
+    : QObject(parent)
+    , m_soundFile(QString())
+    , m_state(StoppedState)
+{
+
+}
+
+Recorder::~Recorder()
+{
+    // nothing to do
+}
+
+void Recorder::setSoundFile(const KUrl &fileUrl)
+{
+    if (!fileUrl.isValid() || fileUrl.isEmpty()) {
+        kWarning() << "Not setting empty sound file path.";
+        return;
+    }
+    m_soundFile = fileUrl;
+    emit soundFileChanged();
+}
+
+void Recorder::setSoundFile(const QString& fileUrl)
+{
+    CaptureDeviceController::self().stopCapture();
+    setSoundFile(KUrl::fromLocalFile(fileUrl));
+}
+
+QString Recorder::soundFile() const
+{
+    return m_soundFile.toLocalFile();
+}
+
+Recorder::RecordingState Recorder::state() const
+{
+    return m_state;
+}
+
+void Recorder::startCapture()
+{
+    if (m_soundFile.isEmpty()) {
+        kError() << "Abort capture, no output file set";
+        return;
+    }
+    kDebug() << this << "Capture to file " << m_soundFile.toLocalFile();
+    m_state = CurrentlyRecordingState;
+    CaptureDeviceController::self().startCapture(m_soundFile.toLocalFile());
+    connect(&CaptureDeviceController::self(), SIGNAL(captureStopped()), this, SLOT(updateState()));
+    emit stateChanged();
+}
+
+void Recorder::stop()
+{
+    CaptureDeviceController::self().stopCapture();
+    CaptureDeviceController::self().disconnect();
+    emit stateChanged();
+}
+
+void Recorder::updateState()
+{
+    if (!CaptureDeviceController::self().state() == CaptureDeviceController::StoppedState) {
+        return;
+    }
+    if (state() == CurrentlyRecordingState) {
+        m_state = StoppedState;
+        emit stateChanged();
+    }
+    CaptureDeviceController::self().disconnect();
+}
---
+++ src/core/recorder.h
@@ -0,0 +1,64 @@
+/*
+ *  Copyright 2013  Andreas Cord-Landwehr <cordlandwehr@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License or (at your option) version 3 or any later version
+ *  accepted by the membership of KDE e.V. (or its successor approved
+ *  by the membership of KDE e.V.), which shall act as a proxy
+ *  defined in Section 14 of version 3 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RECORDER_H
+#define RECORDER_H
+
+#include "artikulatecore_export.h"
+#include <QObject>
+#include <KUrl>
+
+class ARTIKULATELIB_EXPORT Recorder : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString soundFileUrl READ soundFile WRITE setSoundFile NOTIFY soundFileChanged)
+    Q_PROPERTY(RecordingState state READ state NOTIFY stateChanged)
+
+public:
+    Q_ENUMS(RecordingState)
+    enum RecordingState {
+        CurrentlyRecordingState,
+        StoppedState,
+    };
+
+    explicit Recorder(QObject *parent = 0);
+    ~Recorder();
+
+    Q_INVOKABLE void startCapture();
+    Q_INVOKABLE void stop();
+    RecordingState state() const;
+    void setSoundFile(const KUrl &fileUrl);
+    void setSoundFile(const QString &fileUrl);
+    QString soundFile() const;
+
+Q_SIGNALS:
+    void stateChanged();
+    void soundFileChanged();
+
+private Q_SLOTS:
+    void updateState();
+
+private:
+    Q_DISABLE_COPY(Recorder)
+    KUrl m_soundFile;
+    RecordingState m_state;
+};
+
+#endif // RECORDER_H
