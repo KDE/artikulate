6533a4030a7f1a0392d0b3e57b37ce4abe026b29
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2014-01-11 14:02:11 +0100

    Make constructor explicit.

--- src/core/resources/resourceinterface.h
+++ src/core/resources/resourceinterface.h
@@ -40,7 +40,7 @@ public:
         SkeletonResourceType
     };
 
-    ResourceInterface(ResourceManager *resourceManager);
+    explicit ResourceInterface(ResourceManager *resourceManager);
 
     virtual ~ResourceInterface();
 
