6f21ec4818c604dfe38ba9f7e5491cf835bbad95
Pino Toscano <pino@kde.org>
2014-03-24 09:57:50 +0100

    i18n: fix message extraction
    Extract messages from all the directories, including liblearnerprofile
    (which has messages too).
    
    This brings 2 new messages to artikulate.pot.
    CCMAIL: kde-i18n-doc@kde.org

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-$EXTRACTRC `find src -name '*.rc'` >> rc.cpp
-$EXTRACTRC `find src/ui -name *.ui` >> rc.cpp
-$EXTRACTRC `find src -name '*.kcfg'` >> rc.cpp
+$EXTRACTRC `find . -name '*.rc'` >> rc.cpp
+$EXTRACTRC `find . -name *.ui` >> rc.cpp
+$EXTRACTRC `find . -name '*.kcfg'` >> rc.cpp
 $EXTRACTRC --context="Language Title" --tag-group=none --tag=i18nTitle `find data/languages/ -name '*.xml'` >> rc.cpp
-$XGETTEXT rc.cpp `find src/ -name \*.qml -or -name \*.cpp` -o $podir/artikulate.pot
+$XGETTEXT rc.cpp `find . -name \*.qml -or -name \*.cpp` -o $podir/artikulate.pot
