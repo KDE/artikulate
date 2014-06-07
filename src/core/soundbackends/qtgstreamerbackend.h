0723b8d3cee9b51859f4181eca716834ed795b82
Avik Pal <avikpal.me@gmail.com>
2014-06-08 00:35:30 +0530

    Ported to GStreamer 1.0
    Artikulate has been ported to QtGstreamer 1.0 and it no longer supports QtMobility.

--- src/core/soundbackends/qtgstreamerbackend.h
+++ src/core/soundbackends/qtgstreamerbackend.h
@@ -26,7 +26,6 @@
 #include <QString>
 
 #include <QGst/global.h>
-#include <QGst/PropertyProbe>
 #include <QGst/Pipeline>
 
 class QtGStreamerBackend : public SoundBackendInterface
@@ -52,7 +51,6 @@ private:
     void stopPipeline();
     QGst::BinPtr createAudioSrcBin();
 
-    QGst::PropertyProbePtr m_audioProbe;
     QGst::PipelinePtr m_pipeline;
     QString m_device;
     QMap<QString, QString> m_availableDevices; //!> (identifier,human readable name)
