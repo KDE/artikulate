1eb3bd0135f48863f7b6f6cc1b08471927520aed
Laurent Montel <montel@kde.org>
2019-12-03 13:26:23 +0100

    Don't use nullptr as flag value

--- src/qmlcontrols/imagetexturescache.cpp
+++ src/qmlcontrols/imagetexturescache.cpp
@@ -65,5 +65,5 @@ QSharedPointer<QSGTexture> ImageTexturesCache::loadTexture(QQuickWindow *window,
 
 QSharedPointer<QSGTexture> ImageTexturesCache::loadTexture(QQuickWindow *window, const QImage &image)
 {
-    return loadTexture(window, image, nullptr);
+    return loadTexture(window, image, {});
 }
