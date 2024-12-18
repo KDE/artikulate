347748691ad8bfed85302edf60703cea5fe74093
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2024-12-18 19:46:27 +0100

    Rework main drawer by introducing tree model

---
+++ autotests/unittests/drawercoursetreemodel/test_drawercoursetreemodel.cpp
@@ -0,0 +1,165 @@
+/*
+    SPDX-FileCopyrightText: 2024 Andreas Cord-Landwehr <cordlandwehr@kde.org>
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#include "test_drawercoursetreemodel.h"
+#include "../mocks/coursestub.h"
+#include "../mocks/languagestub.h"
+#include "org/kde/artikulate/drawercoursetreemodel.h"
+#include "org/kde/artikulate/trainingaction.h"
+#include "org/kde/artikulate/trainingsession.h"
+#include "src/core/icourse.h"
+#include "src/core/language.h"
+#include "src/core/unit.h"
+#include <QAbstractItemModelTester>
+#include <QSignalSpy>
+#include <QTest>
+
+// assumption: during a training session the units and phrases of a course do not change
+//   any change of such a course shall result in a reload of a training session
+
+void TestDrawerCourseTreeModel::init()
+{
+    // no initialization of test case
+}
+
+void TestDrawerCourseTreeModel::cleanup()
+{
+    // no cleanup after test run
+}
+
+void TestDrawerCourseTreeModel::createTrainingSessionWithoutUnits()
+{
+    auto language = std::make_shared<LanguageStub>("de");
+    CourseStub course(language, QVector<std::shared_ptr<Unit>>());
+
+    DrawerCourseTreeModel model;
+    QAbstractItemModelTester tester(&model);
+    model.setCourse(&course);
+    QCOMPARE(model.rowCount(), 0); // no units
+}
+
+void TestDrawerCourseTreeModel::createTrainingSessionWithBasicUnits()
+{
+    auto language = std::make_shared<LanguageStub>("de");
+    auto unitA = Unit::create();
+    auto unitB = Unit::create();
+    std::shared_ptr<Phrase> phraseA1 = Phrase::create();
+    std::shared_ptr<Phrase> phraseB1 = Phrase::create();
+    // note: phrases without soundfiles are skipped in session generation
+    phraseA1->setId("A1");
+    phraseB1->setId("B1");
+    phraseA1->setSound(QUrl::fromLocalFile("/tmp/a1.ogg"));
+    phraseB1->setSound(QUrl::fromLocalFile("/tmp/b1.ogg"));
+    unitA->addPhrase(phraseA1, unitA->phrases().size());
+    unitB->addPhrase(phraseB1, unitB->phrases().size());
+
+    CourseStub course(language, QVector<std::shared_ptr<Unit>>({unitA, unitB}));
+
+    DrawerCourseTreeModel model;
+    QAbstractItemModelTester tester(&model);
+    model.setCourse(&course);
+    QCOMPARE(model.rowCount(), 2); // units
+}
+
+void TestDrawerCourseTreeModel::createTrainingSessionWithEmptySounds()
+{
+    auto language = std::make_shared<LanguageStub>("de");
+    auto unitA = Unit::create();
+    auto unitB = Unit::create();
+    std::shared_ptr<Phrase> phraseA1 = Phrase::create();
+    std::shared_ptr<Phrase> phraseA2 = Phrase::create();
+    std::shared_ptr<Phrase> phraseB1 = Phrase::create();
+    // note: phrases without soundfiles are skipped in session generation
+    phraseA1->setId("A1");
+    phraseA2->setId("A2");
+    phraseB1->setId("B1");
+    phraseA1->setSound(QUrl::fromLocalFile("/tmp/a1.ogg"));
+    unitA->addPhrase(phraseA1, unitA->phrases().size());
+    unitA->addPhrase(phraseA2, unitA->phrases().size());
+    unitB->addPhrase(phraseB1, unitB->phrases().size());
+
+    CourseStub course(language, QList<std::shared_ptr<Unit>>({unitA, unitB}));
+    DrawerCourseTreeModel model;
+    QAbstractItemModelTester tester(&model);
+    model.setCourse(&course);
+
+    // test number of actions
+    model.setTrainingFilter(true);
+    QCOMPARE(model.rowCount(QModelIndex()), 1);
+    QCOMPARE(model.rowCount(model.index(0, 0)), 1);
+
+    model.setTrainingFilter(false);
+    QCOMPARE(model.rowCount(QModelIndex()), 2);
+    QCOMPARE(model.rowCount(model.index(0, 0)), 2);
+    QCOMPARE(model.rowCount(model.index(1, 0)), 1);
+}
+
+void TestDrawerCourseTreeModel::createTrainingSessionWithEmptyUnits()
+{
+    auto language = std::make_shared<LanguageStub>("de");
+    auto unitA = Unit::create();
+    auto unitB = Unit::create();
+    CourseStub course(language, QVector<std::shared_ptr<Unit>>({unitA, unitB}));
+    DrawerCourseTreeModel model;
+    QAbstractItemModelTester tester(&model);
+    model.setCourse(&course);
+
+    model.setTrainingFilter(true);
+    QCOMPARE(model.rowCount(QModelIndex()), 0);
+
+    model.setTrainingFilter(false);
+    QCOMPARE(model.rowCount(QModelIndex()), 2);
+}
+
+void TestDrawerCourseTreeModel::iterateCourse()
+{
+    auto language = std::make_shared<LanguageStub>("de");
+    auto unitA = Unit::create();
+    auto unitB = Unit::create();
+    std::shared_ptr<Phrase> phraseA1 = Phrase::create();
+    std::shared_ptr<Phrase> phraseA2 = Phrase::create();
+    std::shared_ptr<Phrase> phraseA3 = Phrase::create();
+    std::shared_ptr<Phrase> phraseB1 = Phrase::create();
+    std::shared_ptr<Phrase> phraseB2 = Phrase::create();
+    // note: phrases without soundfiles are skipped in session generation
+    phraseA1->setId("A1");
+    phraseA2->setId("A2");
+    phraseA3->setId("A3");
+    phraseB1->setId("B1");
+    phraseB2->setId("B2");
+    phraseA1->setText("A1 text");
+    phraseA2->setText("A2 text");
+    phraseB1->setText("B1 text");
+    phraseB2->setText("B2 text");
+    phraseA1->setSound(QUrl::fromLocalFile("/tmp/a1.ogg"));
+    phraseA2->setSound(QUrl::fromLocalFile("/tmp/a1.ogg"));
+    // no sound for phrase A3
+    phraseB1->setSound(QUrl::fromLocalFile("/tmp/b1.ogg"));
+    phraseB2->setSound(QUrl::fromLocalFile("/tmp/b2.ogg"));
+    unitA->addPhrase(phraseA1, unitA->phrases().size());
+    unitA->addPhrase(phraseA2, unitA->phrases().size());
+    unitA->addPhrase(phraseA3, unitA->phrases().size());
+    unitB->addPhrase(phraseB1, unitB->phrases().size());
+    unitB->addPhrase(phraseB2, unitB->phrases().size());
+
+    CourseStub course(language, QVector<std::shared_ptr<Unit>>({unitA, unitB}));
+    DrawerCourseTreeModel model;
+    QAbstractItemModelTester tester(&model);
+    model.setCourse(&course);
+
+    const QList<QString> testPhrases = {"A1 text", "A2 text", "B1 text", "B2 text"};
+    QList<QString> iteratedPhrases;
+    QModelIndex index;
+    do {
+        index = model.next(index);
+        if (index.isValid()) {
+            iteratedPhrases << model.data(index, Qt::DisplayRole).toString();
+        }
+    } while (index.isValid());
+
+    QCOMPARE(iteratedPhrases, testPhrases);
+}
+
+QTEST_GUILESS_MAIN(TestDrawerCourseTreeModel)
---
+++ src/org/kde/artikulate/drawercoursetreemodel.h
@@ -0,0 +1,151 @@
+// SPDX-FileCopyrightText: 2024 Andreas Cord-Landwehr <cordlandwehr@kde.org>
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
+#ifndef DRAWERCOURSETREEMODEL_H
+#define DRAWERCOURSETREEMODEL_H
+
+#include "core/iunit.h"
+#include "isessionactions.h"
+#include <QAbstractItemModel>
+#include <QAction>
+#include <QObject>
+#include <QQmlEngine>
+#include <optional>
+
+#ifndef OPAQUE_PTR_Course
+#define OPAQUE_PTR_Course
+Q_DECLARE_OPAQUE_POINTER(ICourse *)
+#endif
+
+class SelectionEntry
+{
+public:
+    explicit SelectionEntry() = default;
+    explicit SelectionEntry(const QString &text, std::shared_ptr<IUnit> data, std::shared_ptr<SelectionEntry> parentItem);
+    explicit SelectionEntry(const QString &text, std::shared_ptr<IPhrase> data, std::shared_ptr<SelectionEntry> parentItem);
+
+    void appendChild(std::shared_ptr<SelectionEntry> child);
+
+    std::shared_ptr<SelectionEntry> child(int row);
+    int childCount() const;
+    int columnCount() const;
+    QVariant data(Qt::ItemDataRole role) const;
+    int row() const;
+    std::shared_ptr<SelectionEntry> parentItem();
+    const QAction *action() const;
+    std::shared_ptr<IPhrase> phrase();
+
+private:
+    std::vector<std::shared_ptr<SelectionEntry>> m_childItems;
+    std::weak_ptr<SelectionEntry> m_parentItem;
+    QString m_text; //!< user formatted string
+    std::optional<std::shared_ptr<IUnit>> m_unit;
+    std::optional<std::shared_ptr<IPhrase>> m_phrase;
+    QAction m_action;
+};
+
+class DrawerCourseTreeModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(ICourse *course READ course WRITE setCourse NOTIFY courseChanged)
+    Q_PROPERTY(ISessionActions *session READ session WRITE setSession NOTIFY sessionChanged)
+    Q_PROPERTY(bool trainingFilter READ trainingFilter WRITE setTrainingFilter NOTIFY trainingFilterChanged)
+
+    QML_ELEMENT
+
+public:
+    enum Roles {
+        Text = Qt::DisplayRole,
+        ToolTip = Qt::ToolTipRole,
+        Selected = Qt::CheckStateRole,
+        Action = Qt::UserRole,
+    };
+    Q_ENUM(Roles)
+
+    /**
+     * @brief Create filter tree model all units and phrases in a course
+     *
+     * @param parent the QObject parent
+     */
+    explicit DrawerCourseTreeModel(QObject *parent = nullptr);
+
+    /**
+     * @brief Destroys the object
+     */
+    ~DrawerCourseTreeModel() override;
+
+    void setCourse(ICourse *course);
+    ICourse *course() const;
+
+    void setSession(ISessionActions *session);
+    ISessionActions *session() const;
+
+    /**
+     * @brief training filter controls if empty units/phrases shall be hidden
+     *
+     * @note by chaning the training filter the model is rebuild. also an enabled
+     *       training filter does not react to unit phrase data being changed.
+     * @param trainingFilter the boolean value
+     */
+    void setTrainingFilter(bool trainingFilter);
+    bool trainingFilter() const;
+
+    /**
+     * @copydoc QAbstractItemModel::rolesNames()
+     */
+    QHash<int, QByteArray> roleNames() const override;
+
+    /**
+     * @copydoc QAbstractItemModel::index()
+     */
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
+
+    /**
+     * @copydoc QAbstractItemModel::parent()
+     */
+    QModelIndex parent(const QModelIndex &index) const override;
+
+    /**
+     * @copydoc QAbstractItemModel::rowCount()
+     */
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    /**
+     * @copydoc QAbstractItemModel::columnCount()
+     */
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    /**
+     * @copydoc QAbstractItemModel::data()
+     */
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    /**
+     * @brief Iteration operator that computes next index when traversing phrases
+     * @return next phrase or invalid model index if none exists
+     */
+    QModelIndex next(const QModelIndex &index) const;
+
+Q_SIGNALS:
+    void courseChanged();
+    void sessionChanged();
+    void trainingFilterChanged();
+    /**
+     * \brief forwarded signal of underlaying session that requests a changed currentIndex \p index
+     */
+    void currentIndexChanged(QModelIndex index);
+
+private Q_SLOTS:
+    void handleSessionPhraseChanged();
+
+private:
+    QModelIndex firstPhraseIndexInUnit(const QModelIndex &index) const;
+    void rebuildModel();
+    std::shared_ptr<SelectionEntry> m_rootItem;
+    ICourse *m_course{nullptr};
+    ISessionActions *m_session{nullptr};
+    bool m_trainingFilter{true};
+};
+
+#endif
